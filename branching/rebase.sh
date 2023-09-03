0f8708665025dccf46fe39a3f72c259c7bf72df7
vyacheslav sadov <sadov.vs33@gmail.com>
2023-09-03 21:46:31 +0300

    git-rebase commit

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -2,8 +2,10 @@
 # display command line options
 
 count=1
+
 for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+    echo "Next parameter: $param"
+
     count=$(( $count + 1 ))
 done
 
